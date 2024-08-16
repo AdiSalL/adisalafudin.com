6f0ac6435f3893417fd85629fd5e9204f8b5d81d
adisalafudin <adiefsal@gmail.com>
2024-08-16 20:37:13 +0700

    final v1: ;

--- src/Components/Contact/Form.jsx
+++ src/Components/Contact/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import emailjs from "emailjs-com";
 function ContactForm() {
   const [formData, setFormData] = useState({
     email: "",
@@ -17,43 +17,70 @@ function ContactForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Code to send the email will go here
+
+    emailjs
+      .sendForm(
+        "service_t744oyd", // Replace with your EmailJS service ID
+        "template_vav2ydy", // Replace with your EmailJS template ID
+        e.target,
+        "Z09IQXlaOf50G7R5W" // Replace with your EmailJS user ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full">
-      <div className="">
-        <label className="input flex items-center gap-2">Email:</label>
-        <input
-          className="input input-bordered w-full max-w-screen-lg"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
+    <div className="mt-2 w-screen h-screen flex flex-col justify-center items-center">
       <div>
-        <label>Subject:</label>
-        <input
-          type="text"
-          name="subject"
-          value={formData.subject}
-          onChange={handleChange}
-          required
-        />
+        <h1>Contact Me:</h1>
       </div>
-      <div>
-        <label>Message:</label>
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Send Email</button>
-    </form>
+      <form onSubmit={handleSubmit} className="w-1/2 mx-auto">
+        <div className="">
+          <label className="input flex items-center">Email:</label>
+          <input
+            className="input input-bordered w-full max-w-screen"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            placeholder="email@email.com"
+          />
+        </div>
+        <div>
+          <label className="input flex items-center">Subject:</label>
+          <input
+            type="text"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+            className="input input-bordered w-full max-w-screen"
+            placeholder="Hiring For Project ...."
+          />
+        </div>
+        <div>
+          <label className="input flex items-center">Message:</label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+            className="textarea textarea-bordered w-full"
+            placeholder="The Project Is To Make SAAS....."
+          />
+        </div>
+        <button type="submit" className="btn w-full">
+          Send Email
+        </button>
+      </form>
+    </div>
   );
 }
 
